48746bee92ab8740250638a59d69b91395f6936b
borjaguiles <borjaguiles@hotmail.com>
2021-05-21 20:18:37 +0200

    feat: generate a reel fro ma file

---
+++ ReelWords/Reel/LetterReelGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using LanguageExt;
+
+namespace ReelWords.Reel
+{
+    public class LetterReelGenerator : ILetterReelGenerator
+    {
+        private const char Separator = ' ';
+
+        public LetterReel GenerateAReel()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "ReelWords.Resources.reels.txt";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                var lines = new List<char[]>();
+                while (!reader.EndOfStream)
+                {
+                    lines.Add(reader.ReadLine().Split(Separator).Select(s => s[0]).ToArray());
+                }
+                return new LetterReel(lines.ToArray());
+            }
+        }
+    }
+}
---
+++ ReelWordsTests/LetterReelGeneratorShould.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using ReelWords;
+using ReelWords.Reel;
+using Xunit;
+
+namespace ReelWordsTests
+{
+    public class LetterReelGeneratorShould
+    {
+        private ILetterReelGenerator _reelGenerator;
+
+        public LetterReelGeneratorShould()
+        {
+            _reelGenerator = new LetterReelGenerator();
+        }
+
+        [Fact]
+        public void GenerateANewReelUsingAFile()
+        {
+            //Assert
+            var expectedLine = new ReelLine(new []{'u','d','x','c','l','a','e'});
+            var reel = _reelGenerator.GenerateAReel();
+            reel.GetActiveLine().Should().BeEquivalentTo(expectedLine);
+        }
+    }
+}
