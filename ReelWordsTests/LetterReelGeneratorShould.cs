cf59bf514f1e8f2b8f67a1778bc50e2829c0d5e5
borjaguiles <borjaguiles@hotmail.com>
2021-05-21 20:23:27 +0200

    test: improve test for hipocondriacs

--- ReelWordsTests/LetterReelGeneratorShould.cs
+++ ReelWordsTests/LetterReelGeneratorShould.cs
@@ -21,8 +21,12 @@ namespace ReelWordsTests
         {
             //Assert
             var expectedLine = new ReelLine(new []{'u','d','x','c','l','a','e'});
+            var expectedFourthIteration = new ReelLine(new []{'u','b','x','c','s','e','h'});
             var reel = _reelGenerator.GenerateAReel();
             reel.GetActiveLine().Should().BeEquivalentTo(expectedLine);
+            reel.MoveSlots(new UserWord("la"));
+            reel.MoveSlots(new UserWord("doe"));
+            reel.MoveSlots(new UserWord("duet"));
         }
     }
 }
