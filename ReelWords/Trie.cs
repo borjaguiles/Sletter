f6051862b02da78c27385e9fb4860d71482ecaf0
borjaguiles <borjaguiles@hotmail.com>
2021-05-21 21:48:27 +0200

    feat: search for a word in a trie

--- ReelWords/Trie.cs
+++ ReelWords/Trie.cs
@@ -21,24 +21,40 @@ namespace ReelWords
             _value = letter;
         }
 
-        public bool Search(string s)
+        public bool Search(string word)
         {
-            throw new NotImplementedException();
+            if (word.Length == 0)
+            {
+                word = EndOfWordCharacter;
+            }
+            var next = _children.FirstOrDefault(s => s == word[0]);
+            
+            if (next == EndOfWordCharacter[0])
+            {
+                return true;
+            }
+
+            if (next == null)
+            {
+                return false;
+            }
+
+            return next.Search(word.Substring(1));
         }
 
         public void Insert(string newWord)
         {
-            if (newWord == EndOfWordCharacter)
-            {
-                return;
-            }
-
             Trie son;
             son = _children.FirstOrDefault(s => s == newWord[0]);
             if (son == null)
             {
                 son = new Trie(newWord[0]);
                 _children.Add(son);
+            }
+
+            if (newWord == EndOfWordCharacter)
+            {
+                return;
             }
 
             var nextSegment = newWord.Substring(1);
